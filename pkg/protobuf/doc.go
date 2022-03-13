375524be6fdb90f57530fc2632118d37ba8b15bb
Koichi Shiraishi <zchee.io@gmail.com>
2022-03-13 13:03:26 +0900

    protobuf: add protobuf package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ pkg/protobuf/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2022 The Go Language Server Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package protobuf generates Protocol Buffers schema from the intermediate representation passed by the transpiler.
+package protobuf
