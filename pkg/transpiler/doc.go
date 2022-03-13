cd23be13d72be3dc020e0e3ffb0f54aa55ea18aa
Koichi Shiraishi <zchee.io@gmail.com>
2022-03-13 12:56:12 +0900

    transpiler: add transpiler package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ pkg/transpiler/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2022 The Go Language Server Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package transpiler provides the transpiles into Protocol Buffers structure from the OpenAPI definitions.
+package transpiler
