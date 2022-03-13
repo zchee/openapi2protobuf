da5067b4ec5caed4c7435fd03623545bdfb63fc8
Koichi Shiraishi <zchee.io@gmail.com>
2022-03-13 13:01:17 +0900

    openapi: add openapi package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ pkg/openapi/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2022 The Go Language Server Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package openapi provides parses OpenAPI definitions and passes them to the transpiler.
+package openapi
