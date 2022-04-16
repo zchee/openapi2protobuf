64718b76d4d0d622b6ad657562b6a0743b6e62de
Koichi Shiraishi <zchee.io@gmail.com>
2022-04-16 22:16:18 +0900

    pkg/compiler: rename to compiler package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ pkg/compiler/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2022 The Go Language Server Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package compiler provides the compiles into Protocol Buffers structure from the OpenAPI definitions.
+package compiler
